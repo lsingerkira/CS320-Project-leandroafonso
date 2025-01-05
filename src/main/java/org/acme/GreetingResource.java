c073c73810a4ed50d96d41d09d7aa98c35d6e0e2
lsingerkira <lsingerafonso@gmail.com>
2025-01-05 14:41:47 -0700

    First Week - personalized HelloPost

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,47 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Path("/hello")
+@Path("hello")
 public class GreetingResource {
 
     @GET
+    @Path("/personalized/{name}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public String personalizedHello(@PathParam("name") String name) {
+        return "Hello " + name;
+    }
+
+    @POST
+    @Path("/personalized")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String personalizedHelloPost(Person p) {
+        return "Hello " + p.getFirst() + " " + p.getLast();
+    }
+
+    public static class Person {
+        @JsonProperty("firstName")
+        private String first;
+        @JsonProperty("lastName")
+        private String last;
+
+        public String getFirst() {
+            return first;
+        }
+
+        public void setFirst(String first) {
+            this.first = first;
+        }
+
+        public String getLast() {
+            return last;
+        }
+
+        public void setLast(String last) {
+            this.last = last;
+        }
     }
 }
