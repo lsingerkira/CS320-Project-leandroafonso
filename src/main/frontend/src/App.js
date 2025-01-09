b875512e0aafc498e90fd199f7f282adcd2f3bea
lsingerkira <lsingerafonso@gmail.com>
2025-01-09 15:17:48 -0700

    Second Week - Changed to 17. Installed SAM

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { API_URL } from "./config"; // Gets the API_URL from the config.js file
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState(""); // Will hold the message from the backend
+
+    // "useEffect" is a React hook that runs after the component is mounted
+    useEffect(() => {
+        const fetchMessage = async () => {
+                const response = await fetch(`${API_URL}/hello/personalized`, {
+                    method: "POST", // POST request
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ firstName: "Ensign", lastName: "Student" }), // JSON body with attributes for a Person
+                });
+                const text = await response.text(); // Waits for the response and then converts it to text
+                setMessage(text); // Sets the message with the text from the response
+        };
+        fetchMessage();
+    }, []); // Dependency array ensures this runs only once when the component mounts
+
+    return (
+        <div>
+            <h1>Message from the Backend:</h1>
+            <p>{message}</p>
+        </div>
+    );
 }
 
 export default App;
