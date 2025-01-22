016208dbcbe1c328a9f8eb259ef3f69d7de45d51
lsingerkira <lsingerafonso@gmail.com>
2025-01-22 12:48:25 -0700

    Week 4 - Page2

---
+++ src/main/frontend/src/Page2.js
@@ -0,0 +1,9 @@
+function Page2() {
+    return (
+        <div>
+            <h1>Page 2! Lets do it!!</h1>
+        </div>
+    );
+}
+
+export default Page2;
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Page2 from './Page2';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App/>}/>
+              <Route path="/page2" element={<Page2/>}/>
+              {/*Here we add more pages by adding more Route elements*/}
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
